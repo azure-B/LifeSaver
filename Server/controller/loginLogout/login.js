5b4bce152d18928fae439707c8a3a594f7b8984f
HowToBasicDD <benefit5758@naver.com>
2023-07-03 12:27:41 +0900

    Refactoring Diretory

---
+++ Server/controller/loginLogout/login.js
@@ -0,0 +1,39 @@
+const Module = require("../../Model/resgister,login/loginModule");
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  const comparePw = await Module.comparePw(email, password);
+  const compareEmail = await Module.compareEmail(email);
+
+  const message = {
+    error: "아이디 혹은 비밀번호를 확인해 주세요",
+    success: "로그인 성공",
+  };
+
+  if (!compareEmail || !comparePw) {
+    return res.send({ message: message.error });
+  } else {
+    try {
+      const { name } = await Module.userInfo(email);
+      req.session.user = {
+        email,
+        name,
+      };
+
+      req.session.save((err) => {
+        if (err) {
+          console.error("세션 저장 오류:", err);
+        } else {
+          console.log("세션 저장 완료");
+        }
+      });
+
+      return res.status(200).send({ message: message.success, result: true });
+    } catch (error) {
+      console.error("에러가 발생했습니다:", error);
+      return res
+        .status(500)
+        .send({ message: "에러가 발생했습니다.", result: false });
+    }
+  }
+};
---
+++ Server/controller/register/emailCertify.js
@@ -0,0 +1,85 @@
+const { response } = require("express");
+const Module = require("../../Model/resgister,login/emailCertifyModule");
+const models = require("../../models");
+
+exports.MailSender = async (req, res) => {
+  const { email } = req.body;
+  const auth = Module.getRandomKey(111111, 999999);
+  const idDupleCk = await Module.idDuplicateCk(email);
+
+  const response = {
+    resMsg: null,
+    resResult: true,
+  };
+
+  const message = {
+    error: "이미 존재하는 아이디 입니다.",
+    success: "메일이 전송되었습니다.",
+  };
+
+  const emailParam = {
+    toEmail: email, // 수신할 이메일
+    subject: "[ LifeSaver ] 인증 관련 메일입니다. ", // 메일 제목
+    text: `오른쪽 숫자 6자리를 입력해 주세요: ${auth}`, // 메일 내용
+  };
+
+  try {
+    if (idDupleCk) {
+      response.resMsg = message.error;
+      response.resResult = false;
+    } else {
+      const authData = await models.db.auth.findOne({ where: { email } });
+
+      if (authData) {
+        await models.db.auth.update({ auth }, { where: { email } });
+      } else {
+        await models.db.auth.create({ email, auth });
+      }
+
+      await Module.sendMail(emailParam);
+
+      response.resMsg = message.success;
+      response.resResult = true;
+    }
+  } catch (error) {
+    response.resMsg = "에러가 발생했습니다.";
+    response.resResult = false;
+    console.error(error);
+  }
+
+  res.send({ message: response.resMsg, result: response.resResult });
+};
+
+exports.mailVerifier = async (req, res) => {
+  const { email, auth } = req.body;
+
+  const message = {
+    error: "키를 다시한번 확인해주세요.",
+    success: "이메일 인증 성공!",
+  };
+
+  const response = {
+    resMsg: null,
+    resResult: true,
+  };
+
+  try {
+    const result = await models.db.auth.findOne({ where: { email } });
+
+    if (result.auth === auth) {
+      await models.db.auth.update({ auth_flag: "Y" }, { where: { email } });
+
+      response.resMsg = message.success;
+      response.resResult = true;
+    } else {
+      response.resMsg = message.error;
+      response.resResult = false;
+    }
+  } catch (error) {
+    response.resMsg = "에러가 발생했습니다.";
+    response.resResult = false;
+    console.error(error);
+  }
+
+  res.send({ message: response.resMsg, result: response.resResult });
+};
