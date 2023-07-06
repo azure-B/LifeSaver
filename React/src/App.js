9b9878f80a71eced2a5cc3b9311a9821209a9add
HowToBasicDD <benefit5758@naver.com>
2023-07-06 14:53:50 +0900

    regain

--- React/src/App.js
+++ React/src/App.js
@@ -1,23 +1,13 @@
-<<<<<<< HEAD
 import CheckImg from "./components/CheckImg";
 import DetailsPageAll from "./components/DetailsPageAll";
 import Login from "./components/Login";
 import Logout from "./components/Logout";
 import Regi from "./components/Registers";
-=======
-import Footer from "./components/Layout/Footer";
-import Menu from "./components/Menu";
->>>>>>> 4a550f2 (7월 6일 메뉴, 푸터)
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <DetailsPageAll />
-=======
-      {/* <Menu /> */}
-      <Footer />
->>>>>>> 4a550f2 (7월 6일 메뉴, 푸터)
     </>
   );
 }
