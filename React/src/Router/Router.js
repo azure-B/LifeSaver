74d0ebc739fb923cd107849362f091e303b7309e
dpflsy25 <dpflsy25@gmail.com>
2023-07-07 09:34:52 +0900

    fix: DevBranch를 위한 작업

--- React/src/Router/Router.js
+++ React/src/Router/Router.js
@@ -1,10 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Posts from "./../Components/Posts/Posts";
+import PostList from "./../Components/Posts/PostList";
+import PostDetail from "./../Components/Posts/PostDetail";
+import PostWrite from "./../Components/Posts/PostWrtie";
+import PostEdit from "./../Components/Posts/PostEdit";
 
 const RouterData = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/NoticeBoard",
+        element: <Posts />,
+        children: [
+          { path: "", element: <PostList /> },
+          { path: ":postId", element: <PostDetail /> },
+          { path: "write", element: <PostWrite /> },
+          { path: "edit/:postId", element: <PostEdit /> },
+        ],
+      },
+    ],
   },
 ]);
 
