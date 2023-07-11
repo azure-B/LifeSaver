8975e6c90f4863ec1d1dc8a25522cd67001d9f94
HowToBasicDD <benefit5758@naver.com>
2023-07-11 15:01:26 +0900

    Server Error Solved

--- React/src/components/ScrollBackground.js
+++ React/src/components/ScrollBackground.js
@@ -97,19 +97,19 @@ const ScrollBackground = () => {
 
   const videosArray = [
     {
-      video: "videos-main.MP4",
+      video: "videos-main.mp4",
       text: "소멸의 위기 속에서 희망을 찾다\n생명지킴이",
     },
     {
-      video: "videos-wolf.MP4",
+      video: "videos-wolf.mp4",
       text: "한국의 멸종 위기 동물들은\n\n매년 수치가 증가하고 있어\n 보호 조치가 시급한 상황입니다.",
     },
     {
-      video: "videos-bird.MP4",
+      video: "videos-bird.mp4",
       text: "여러 단체와 정부는\n\n 인식 개선 교육과 경쟁력 있는 정책을 구축하여\n 생태계의 균형을 찾으려 노력하고 있습니다.",
     },
     {
-      video: "videos-fish.MP4",
+      video: "videos-fish.mp4",
       text: "우리 모두가 적극적인 환경 보호하여\n\n멸종 위기 동물들이 안전하게 서식할 수 있는\n지구를 만들어 갈 필요가 있습니다.",
     },
   ];
